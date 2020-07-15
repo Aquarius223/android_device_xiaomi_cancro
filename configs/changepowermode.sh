452238cd653baa6dd991acef1eda660c2d3c4f67
Amktiao <47409494+Aquarius223@users.noreply.github.com>
2020-07-15 18:17:38 +0800

    Optimize scheduling

--- configs/changepowermode.sh
+++ configs/changepowermode.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2020 Amktiao.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +20,6 @@ powermode=`getprop sys.perf.profile`
 dev_governor=`ls /sys/class/devfreq/qcom,cpubw*/governor`
 
 case "$powermode" in
-    "2")
-        #Performance
         stop mpdecision
         echo 1            > /sys/devices/system/cpu/cpu0/online
         echo 1            > /sys/devices/system/cpu/cpu1/online
@@ -31,9 +30,9 @@ case "$powermode" in
         echo performance  > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
         echo performance  > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
         echo performance   > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-        echo 20            > /sys/module/cpu_boost/parameters/boost_ms
+        echo 80            > /sys/module/cpu_boost/parameters/boost_ms
         echo 1497600       > /sys/module/cpu_boost/parameters/input_boost_freq
-        echo 40            > /sys/module/cpu_boost/parameters/input_boost_ms
+        echo 120            > /sys/module/cpu_boost/parameters/input_boost_ms
         echo 1728000       > /sys/module/cpu_boost/parameters/sync_threshold
         echo 578000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
         echo performance   > /sys/class/kgsl/kgsl-3d0/devfreq/governor
@@ -60,7 +59,6 @@ case "$powermode" in
         esac
         ;;
     "1")
-        #Balanced
         echo 1                                   > /sys/devices/system/cpu/cpu0/online
         echo 1                                   > /sys/devices/system/cpu/cpu1/online
         echo 1                                   > /sys/devices/system/cpu/cpu2/online
@@ -70,11 +68,11 @@ case "$powermode" in
         echo interactive                         > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
         echo interactive                         > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
         echo interactive                         > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-        echo "19000 1400000:39000 1700000:19000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-        echo 99                                  > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+        echo "16000 1400000:42000 1500000:38000 1700000:19000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+        echo 95                                  > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
         echo 1497600                             > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
         echo 40000                               > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-        echo 20                                  > /sys/module/cpu_boost/parameters/boost_ms
+        echo 25                                  > /sys/module/cpu_boost/parameters/boost_ms
         echo 1728000                             > /sys/module/cpu_boost/parameters/sync_threshold
         echo 1497600                             > /sys/module/cpu_boost/parameters/input_boost_freq
         echo 40                                  > /sys/module/cpu_boost/parameters/input_boost_ms
@@ -107,10 +105,9 @@ case "$powermode" in
         start mpdecision
         ;;
     "0")
-        #PowerSave
         stop mpdecision
         echo 1             > /sys/devices/system/cpu/cpu0/online
-        echo 0             > /sys/devices/system/cpu/cpu1/online
+        echo 1             > /sys/devices/system/cpu/cpu1/online
         echo 0             > /sys/devices/system/cpu/cpu2/online
         echo 0             > /sys/devices/system/cpu/cpu3/online
         echo 2             > /sys/devices/system/cpu/sched_mc_power_savings
@@ -122,7 +119,7 @@ case "$powermode" in
         echo 0             > /sys/module/cpu_boost/parameters/boost_ms
         echo 960000        > /sys/module/cpu_boost/parameters/sync_threshold
         echo 960000        > /sys/module/cpu_boost/parameters/input_boost_freq
-        echo 40            > /sys/module/cpu_boost/parameters/input_boost_ms
+        echo 60            > /sys/module/cpu_boost/parameters/input_boost_ms
         echo 330000000     > /sys/class/kgsl/kgsl-3d0/max_gpuclk
         echo msm-adreno-tz > /sys/class/kgsl/kgsl-3d0/devfreq/governor
         echo "cpubw_hwmon" > $dev_governor
